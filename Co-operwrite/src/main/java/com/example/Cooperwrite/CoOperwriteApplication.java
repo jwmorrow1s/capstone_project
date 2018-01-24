d01b77a065a4dc189fa6274af0d152422c81ce67
Jeff Morrow <jwmorrow1s@gmail.com>
2018-01-24 14:03:41 -0600

    added an empty database and configured within the application settings

---
+++ Co-operwrite/src/main/java/com/example/Cooperwrite/CoOperwriteApplication.java
@@ -0,0 +1,12 @@
+package com.example.Cooperwrite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoOperwriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoOperwriteApplication.class, args);
+	}
+}
