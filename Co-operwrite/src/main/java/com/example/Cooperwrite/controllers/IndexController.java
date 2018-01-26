22f98d6b7828bc98107155b37be8f1ebfe68ce0d
Jeff Morrow <jwmorrow1s@gmail.com>
2018-01-25 21:52:54 -0600

    Created fragments.html, IndexController, and index.html(currently mostly empty)

---
+++ Co-operwrite/src/main/java/com/example/Cooperwrite/controllers/IndexController.java
@@ -0,0 +1,20 @@
+package com.example.Cooperwrite.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("index")
+public class IndexController {
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String index(Model model){
+        model.addAttribute("title", "hello");
+        return "index/index";
+    }
+}
